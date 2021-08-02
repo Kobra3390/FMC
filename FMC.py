6cf62658dc5fccef3094e9bb78f71f8f49ae6389
Kobra3390 <kobra3310@pop-os.localdomain>
2021-08-02 14:26:34 +0200

    started

---
+++ FMC.py
@@ -0,0 +1,16 @@
+import rich
+from rich.console import Console
+from rich.progress import track
+from function import *
+
+console = Console()
+
+console.print("""[blue] ______   __    __     ______    
+/\  ___\ /\ "-./  \   /\  ___\   
+\ \  __\ \ \ \-./\ \  \ \ \____  
+ \ \_\    \ \_\ \ \_\  \ \_____\ 
+  \/_/     \/_/  \/_/   \/_____/ 
+                                 
+
+""")
+
